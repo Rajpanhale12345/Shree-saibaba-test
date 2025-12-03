3844bba0cc0384afc7ae4b7011d8ea0b77307271
Raj <panhaleraj003@gmail.com>
2025-12-03 15:51:28 +0530

    merge conflict solved

--- src/App.jsx
+++ src/App.jsx
@@ -52,15 +52,11 @@ import Vitamin_B12 from "./Blogs/B12";
 import Heart_Attack from "./Blogs/Heart_Attack";
 import Heart_Disease from "./Blogs/Heart_Disease";
 import Interventional_R from "./Blogs/Interventional_R";
-<<<<<<< HEAD
-import Popup from "./popup";
-=======
 import Pneumonia from "./Blogs/Pneumonia";
 import Lifestyle from "./Blogs/Lifestyle_Changes";
 import Understanding_CABG from "./Blogs/Cabg";
 import Kidney_Health from "./Blogs/Kidney_Health"
 
->>>>>>> fc280b7d12073bf9bdce06ebfc6d4364ce5dd733
 
 function App() {
   return (
@@ -120,14 +116,10 @@ function App() {
           <Route path="/Heart_Attack" element={< Heart_Attack/>} />
           <Route path="/Heart_Disease" element={< Heart_Disease/>} />
           <Route path="/Interventional_Radiology" element={< Interventional_R/>} />
-<<<<<<< HEAD
-          <Route path="/popup" element={<Popup />} />
-=======
           <Route path="/Pneumonia" element={< Pneumonia/>} />
           <Route path="/Lifestyle" element={< Lifestyle/>} />
           <Route path="/Understanding_CABG" element={< Understanding_CABG/>} />
           <Route path="/Kidney_Health" element={< Kidney_Health/>} />
->>>>>>> fc280b7d12073bf9bdce06ebfc6d4364ce5dd733
         </Routes>
         <ScrollToTop />
       </main>
